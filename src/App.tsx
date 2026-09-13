89a6e3f498a4b93be001c25e2d255b0864128425
mashiatm300 <itsmashiat@gmail.com>
2026-09-13 21:14:09 +0600

    add: daisyui, typography, tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+    return(
+        <div className="min-h-screen text-slate-900 flex flex-col bg-[#fafafa]">
+
+        </div>
+    );
 
 };
 
-export default App
+export default App;
